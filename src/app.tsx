4222d0fc10a99a98403322e2385b3418483cd70a
Eduardo Tavares <edutav.dev@gmail.com>
2024-06-02 20:43:59 -0300

    react helmet seo

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,15 @@
 import './global.css'
 
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
 import { routes } from './routes'
 
 export function App() {
-  return <RouterProvider router={routes} />
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <RouterProvider router={routes} />
+    </HelmetProvider>
+  )
 }
