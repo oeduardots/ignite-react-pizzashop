e025bcacb526982ad4f355d75e42cba182a42fdf
Eduardo Tavares <edutav.dev@gmail.com>
2024-06-05 11:13:47 -0300

    loading state no perfil

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -13,18 +13,20 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from './ui/dropdown-menu'
+import { Skeleton } from './ui/skeleton'
 
 export function AccountMenu() {
   // data são os dados que a requisição buscou
-  const { data: profile } = useQuery({
+  const { data: profile, isLoading: isLoadingProfile } = useQuery({
     queryKey: ['profile'], // ele usa isso como chave para fazer a deduplicação de req http. Ex. quando precisamos buscar o perfil de usuário logado em vários lugares da aplicação ele usa isso para identificar que é a mesma req sendo chamada em vários locais
     queryFn: getProfile, // função disparada pra fazer o GET no back-end
   })
 
-  const { data: managedRestaurant } = useQuery({
-    queryKey: ['managed-restaurant'],
-    queryFn: getManagedRestaurant,
-  })
+  const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant } =
+    useQuery({
+      queryKey: ['managed-restaurant'],
+      queryFn: getManagedRestaurant,
+    })
 
   return (
     <DropdownMenu>
@@ -33,16 +35,29 @@ export function AccountMenu() {
           variant="outline"
           className="flex select-none items-center gap-2"
         >
-          {managedRestaurant?.name}
+          {isLoadingManagedRestaurant ? (
+            <Skeleton className="h-4 w-40" />
+          ) : (
+            managedRestaurant?.name
+          )}
           <ChevronDown className="size-4" />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-56">
         <DropdownMenuLabel className="flex flex-col">
-          <span>{profile?.name}</span>
-          <span className="text-xs font-normal text-muted-foreground">
-            {profile?.email}
-          </span>
+          {isLoadingProfile ? (
+            <div className="space-y-1.5">
+              <Skeleton className="h-4 w-32" />
+              <Skeleton className="h-3 w-24" />
+            </div>
+          ) : (
+            <>
+              <span>{profile?.name}</span>
+              <span className="text-xs font-normal text-muted-foreground">
+                {profile?.email}
+              </span>
+            </>
+          )}
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuItem>
--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -6,3 +6,11 @@ export const api = axios.create({
   baseURL: env.VITE_API_URL,
   withCredentials: true, // faz com que o back-end receba os cookies do front-end, para saber se o usuário está logado ou não.
 })
+
+if (env.VITE_ENABLE_API_DELAY) {
+  api.interceptors.request.use(async (config) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    return config
+  })
+}
